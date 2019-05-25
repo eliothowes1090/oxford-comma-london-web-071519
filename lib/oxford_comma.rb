025a0bd9f591448d4ac7353a10ac59f0d6d8d208
Eliot Howes <ejhowes@gmail.com>
2019-05-25 21:54:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size > 2
-    array[array.size - 2] << " and "
+    array.insert(-1, " and ")
     return array
     # array.join(", ")
   end
