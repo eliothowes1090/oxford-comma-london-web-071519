18d29211cc5375bf570f92499248914f499b2aff
Eliot Howes <ejhowes@gmail.com>
2019-05-25 21:58:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,8 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size > 2
-    array.join(", ")
     array.insert(-2, "and")
+    array.join(", ")
   end
 end
 
