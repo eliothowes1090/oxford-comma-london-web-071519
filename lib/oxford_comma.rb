ba3043e50eba0f5a2a1e8f01cefc59012846056f
Eliot Howes <ejhowes@gmail.com>
2019-05-25 21:26:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.size == 1
+    array
+  elsif array.size == 2
+    array.joint(, and)
+
+  array.join(", ")
+  # return array
 end
+
+binding.pry
